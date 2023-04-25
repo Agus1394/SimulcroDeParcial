c5f9af5cd42bd916548d944b9426ad27b9fa5618
Agustina <maldonadoale1394@gmail.com>
2023-04-25 15:16:36 -0300

    subiendo el simulacro de parcial

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int correcta, enBlanco, incorrecta;
+double nota;
+
+Console.WriteLine("Ingresar respuestas correctas:");
+correcta= Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingresar respuestas en blanco:");
+enBlanco= Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingresar respuestas incorrectas:");
+precio= Convert.ToInt32(Console.ReadLine());
+
+nota= correcta - (incorrecta * 0.5);
+
+if (enBlanco > (0.25 * (correcta + enBlanco + incorrecta))){
+    nota= nota - 2;
+}
+
+Console.WriteLine("La nota final es: "+nota);
